e92ef2fc25921c5f8d5e0152f4359aa16bce6407
siddhartha gupta <sgupta2@my.westga.edu>
2018-07-10 23:34:02 -0500

    Added method stub in class AdvancedStrategy

---
+++ src/edu/westga/cs6910/pig/model/strategies/AdvancedStrategy.java
@@ -0,0 +1,20 @@
+package edu.westga.cs6910.pig.model.strategies;
+
+/**
+ * This class creates an advanced play strategy for the game Pig and implementes
+ * PigStrategy
+ * 
+ * @author Siddhartha Gupta
+ * @version 7/10/2018
+ *
+ */
+public class AdvancedStrategy implements PigStrategy {
+
+	@Override
+	public boolean rollAgain(int numberOfRollsSoFar, int pointsSoFarThisTurn, int pointsToGoal,
+			int opponentPointsToGoal) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
