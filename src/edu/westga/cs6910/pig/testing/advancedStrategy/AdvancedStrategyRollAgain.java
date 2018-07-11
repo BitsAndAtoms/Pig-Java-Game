b63ee6090a7233a5a17c3fb71fed6ef4865b67fa
siddhartha gupta <sgupta2@my.westga.edu>
2018-07-10 23:48:26 -0500

    First test method for startOfGame with advanced strategy.

--- src/edu/westga/cs6910/pig/testing/advancedStrategy/AdvancedStrategyRollAgain.java
+++ src/edu/westga/cs6910/pig/testing/advancedStrategy/AdvancedStrategyRollAgain.java
@@ -1,29 +1,33 @@
-/**
- * 
- */
 package edu.westga.cs6910.pig.testing.advancedStrategy;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs6910.pig.model.strategies.AdvancedStrategy;
+
 /**
- * @author Sid
- *
+ * @author Siddhartha Gupta
+ * @version 7/10/2018
  */
 class AdvancedStrategyRollAgain {
+	private AdvancedStrategy playAdvancedStrategy;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@BeforeEach
 	void setUp() throws Exception {
+		this.playAdvancedStrategy = new AdvancedStrategy();
 	}
 
+	/**
+	 * Test for the first roll of the game by the computer
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testShouldReturnTruAtStartOfGame() {
+		boolean result = this.playAdvancedStrategy.rollAgain(0, 0, 100, 100);
+		assertEquals(true, result);
 	}
 
 }
